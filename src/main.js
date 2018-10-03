4ff1b5c5b376727a4418922ec5ebce1898d3e64f
S2 <i@ireoo.com>
2018-10-03 17:22:05 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+// 加载主要组件
+import store from "./store";
+import router from "./router";
+
+// 加载插件
+const files = require.context("./plugins", false, /\.js$/);
+files.keys().forEach(key => {
+	require(`./plugins/${key.replace(/(\.\/|\.js)/g, "")}`);
+});
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	store,
+	render: h => h(App)
+}).$mount("#app");
