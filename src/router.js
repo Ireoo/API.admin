99b1b94ae84d78dc619213178501f866b9fc5764
Jack Wang <jack.wang@integem.com>
2018-11-25 21:02:12 +0800

    修改文件

--- src/router.js
+++ src/router.js
@@ -4,67 +4,65 @@ import Router from "vue-router";
 Vue.use(Router);
 
 const routes = [
-	{
-		name: "Index",
-		path: "/",
-		redirect: "/home",
-		meta: {
-			title: "首页重定向",
-			file: `To /home`
-		}
-	}
+  {
+    name: "Index",
+    path: "/",
+    redirect: "/home",
+    meta: {
+      title: "首页重定向",
+      file: `To /home`
+    }
+  }
 ];
 const files = require.context("./views", true, /\.vue$/);
 
 files.keys().forEach(key => {
-	let file = key.replace(/(\.\/|\.vue)/g, "");
-	let _path = file.replace("#", ":");
+  let file = key.replace(/(\.\/|\.vue)/g, "");
+  let _path = file.replace("#", ":");
 
-	let fileArray = file.split("/");
-	let filename = fileArray
-		.pop()
-		.replace(/\b([a-zA-Z#])(\w*)/g, function($0, $1, $2) {
-			return $1.replace("#", "").toUpperCase() + $2.toLowerCase();
-		})
-		.replace("#", "");
-	let dir = fileArray.join("/");
-	let dirname = fileArray
-		.map(d => {
-			return d.replace(/\b([a-zA-Z#])(\w*)/g, function($0, $1, $2) {
-				return $1.toUpperCase() + $2.toLowerCase();
-			});
-		})
-		.join("");
+  let fileArray = file.split("/");
+  let filename = fileArray
+    .pop()
+    .replace(/\b([a-zA-Z#])(\w*)/g, function($0, $1, $2) {
+      return $1.replace("#", "").toUpperCase() + $2.toLowerCase();
+    })
+    .replace("#", "");
+  let dir = fileArray.join("/");
+  let dirname = fileArray
+    .map(d => {
+      return d.replace(/\b([a-zA-Z#])(\w*)/g, function($0, $1, $2) {
+        return $1.toUpperCase() + $2.toLowerCase();
+      });
+    })
+    .join("");
 
-	let component = require(`@/views/${file}`).default;
+  let component = require(`@/views/${file}`).default;
 
-	let route = {
-		path:
-			filename.toLowerCase() === "index"
-				? `/${dir}`
-				: `/${_path.toLowerCase()}`,
-		name:
-			filename.toLowerCase() === "index" && dirname !== ""
-				? dirname
-				: filename,
-		component: component,
+  let route = {
+    path:
+      filename.toLowerCase() === "index"
+        ? `/${dir}`
+        : `/${_path.toLowerCase()}`,
+    name:
+      filename.toLowerCase() === "index" && dirname !== "" ? dirname : filename,
+    component: component,
 
-		meta: {
-			title: component.title,
-			index: component.index | 0,
-			icon: component.icon || ""
-		}
-	};
+    meta: {
+      title: component.title,
+      index: component.index | 0,
+      icon: component.icon || ""
+    }
+  };
 
-	routes.push(route);
+  routes.push(route);
 
-	if (process.env.NODE_ENV !== "production")
-		// eslint-disable-next-line
-		console.log(
-			`加载文件: ${key} \r\n名称: ${route.name} \r\n挂载点: ${
-				route.path
-			} \r\n完成!`
-		);
+  if (process.env.NODE_ENV !== "production")
+    // eslint-disable-next-line
+    console.log(
+      `加载文件: ${key} \r\n名称: ${route.name} \r\n挂载点: ${
+        route.path
+      } \r\n完成!`
+    );
 });
 
 // routes.push({
@@ -75,12 +73,12 @@ files.keys().forEach(key => {
 // });
 
 routes.push({
-	path: "*",
-	redirect: "/"
+  path: "*",
+  redirect: "/"
 });
 
 const router = {
-	routes
+  routes
 };
-if (process.env.NODE_ENV === "production") router.mode = "history";
+// if (process.env.NODE_ENV === "production") router.mode = "history";
 export default new Router(router);
