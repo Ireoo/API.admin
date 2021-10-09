9f452ce018f88891a05b04d9f9fae9e66c50ba60
Ireoo <s@ireoo.com>
2021-10-09 16:18:26 +0800

    增加本地接口转接，隐藏token

--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
+const fs = require("fs");
 const path = require("path");
+const consola = require("consola");
 
 const router = require("express").Router();
 
@@ -8,6 +10,7 @@ router.auth = true;
 router.path = "/";
 
 router.all("/:table/:mode", async (req, res, next) => {
+  consola.info(req.data);
   res.send({
     success: true,
     data: ""
